d48d7dd6c007e14f2427419f0ad12cf8fbc814f8
AlessandroSemeraro <alesemeraro1994@gmail.com>
2023-12-04 22:59:21 +0100

    added remove function

---
+++ js/script.js
@@ -0,0 +1,55 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            userNewToDo:'',
+
+            toDoList: [
+
+            teamMeeting = {
+            text: 'Set a team meeting on Friday at 2PM.' ,
+            done: true,
+            },
+            
+            scheduleInterview = {
+            text:' Schedule an interview with Ms Parker.',
+            done: false,
+            },
+
+            informTeacher = {
+                text:' Instruct Mr Gino to arrange training material for the newly students.',
+                done: true,
+            },
+
+            orientationProgram = {
+                text:' Discuss the orientation program with HR',
+                done: false,
+            },
+
+            meetingStatusUpdate = {
+                text:' Meet with Mrs Wellington for a status update on the sponsorship proposal',
+                done: true,
+            },
+            ]
+            }
+        },
+    methods: {
+        alreadyDone(elementDone){
+            elementDone.done = false
+        },
+
+        removeElement(index){
+            this.toDoList.splice(index,1)
+        },
+
+        addElement(){
+            this.toDoList.push(userNewToDo)
+        }
+        }
+        }
+
+    
+    
+   ).mount('#app');
+
